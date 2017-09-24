e4c7b474e3f38f4a923693c9c5b5fcd1dc6c6606
Viktor Bogutskii <info@siteograf.com>
2017-09-23 18:00:58 -0700

    bash script to upload files to server

---
+++ img_to_serv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Инициализируем переменные
+
+RemoteSSHUser="root"
+RemoteSSHIP="138.197.209.00"
+RemoteSiteDir="/var/www/html/drupal/sites/default/files/import_files/"
+lclSiteDir="/Users/m/sites/js/node1/in_xml/import_files/"
+
+#===== НА СЕРВЕР ===============================================================================
+
+echo "=== TO SERVER ==="
+echo "=== Очистка папки с картинками на сервере ==="
+ssh es "rm -rf $RemoteSiteDir/*"
+
+echo "=== Синхронизация файлов"
+rsync -avz  --no-perms $lclSiteDir $RemoteSSHUser@$RemoteSSHIP:$RemoteSiteDir
+echo "=== ФАЙЛЫ синхронизирована"
